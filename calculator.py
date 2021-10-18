99b04ddfad2331ba37c9c4f450720f7143d2ff9c
FatemeAlinia <fatemealinia98@gmail.com>
2021-10-18 13:17:44 +0330

    final

--- calculator.py
+++ calculator.py
@@ -1,25 +1,42 @@
+import math
+
 r=0
 num1=float(input('enter the number1: '))
 
 while True :
-    op=input('enter the op:+ , - ,* , /  ')
-    if op=='+'or op=='-' or op=='*' or op=='/':
+    op=input('enter the op:+ , - ,* , /,^ ,sin , cos, tan , cot ,sqrt:  ')
+    if op=='+'or op=='-' or op=='*' or op=='/' or op=='sin' or op=='cos ' or op=='sqrt'  or op=='tan' or op=='cot' or op=='^':
         break
     else:
         print('op is wrong')
+if  op=='+'or op=='-' or op=='*' or op=='/'or op=='^':
 
-num2=float(input('enter the number2: '))
+    num2=float(input('enter the number2: '))
 
-if op=='+': 
-    r=num1+num2
-elif op =='-':
-    r=num1-num2
-elif op=='*':
-    r=num1*num2
-elif op=='/':
-    r=num1/num2
+    if op=='+': 
+        r=num1+num2
+    elif op =='-':
+        r=num1-num2
+    elif op=='*':
+        r=num1*num2
+    elif op=='/':
+        r=num1/num2
+    elif op=='^':
+        r=num1**num2
+    print(str(num1)+op+ str(num2)+'='+str(r))    
+else:
+    if op=='sin': 
+        r=math.sin(num1)
+    elif op =='cos':
+        r=math.cos(num1)
+    elif op=='tan':
+        r=math.tan(num1)
+    elif op=='cot':
+        r=1/math.tan(num1)
+    elif op=='sqrt':
+        r=math.sqrt(num1)
 
-print(str(num1)+op+ str(num2)+'='+str(r))        
+    print(op+' '+ str(num1)+'='+str(r))    
 
     
    
